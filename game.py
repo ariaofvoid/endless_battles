00040ae6af1df7bcb5c1efda0a6a7c2cd5ad93ac
manaf <manaf.taleb@gmail.com>
2018-10-08 12:42:00 +0300

    Basic game loop structure

--- game.py
+++ game.py
@@ -1,8 +1,24 @@
 import sys
 import rpg
 
+
+def check_death(characters):
+    # return true if every character in the list is dead
+    for char in characters:
+        if char.hp != 0:
+            return False
+    return True
+
+
+# print python version & welcome message
 print sys.version
 print "-" * 20 + "\n\n\n"
-
 print "Welcome to Endless Battles!"
+
+# Game setup
 player = rpg.Character("Link", 100)
+foes = [rpg.Character("Undead Knight", 50), rpg.Character("Medusa", 35)]
+
+
+# Game main loop
+#while check_death(foes):
