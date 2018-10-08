0a9b86efe65848b4b904c70e294cfafc01b5dbe9
manaf <manaf.taleb@gmail.com>
2018-10-08 12:47:57 +0300

    Add get_hit() to character

--- rpg.py
+++ rpg.py
@@ -3,3 +3,9 @@ class Character():
         self.name = name
         self.hp = hp
         print "A '{0}' has been born, with {1}hp!".format(name,hp)
+
+    def get_hit(self, damage):
+        self.hp -= damage
+        if self.hp < 0:
+            self.hp = 0
+
