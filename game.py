b7de7a4b8f1469121ada15489cad27584e9808cd
manaf <manaf.taleb@gmail.com>
2018-10-09 08:09:43 +0300

    Convert syntax to Python3

--- game.py
+++ game.py
@@ -11,9 +11,9 @@ def check_death(characters):
 
 
 # print python version & welcome message
-print sys.version
-print "-" * 20 + "\n\n\n"
-print "Welcome to Endless Battles!"
+print(sys.version)
+print("-" * 20 + "\n\n\n")
+print("Welcome to Endless Battles!")
 
 # Game setup
 player = rpg.Character("Link",[6,3,4,2,5,6], 100)
@@ -21,14 +21,14 @@ foes = [rpg.Character("Undead Knight",[2,2,1,1,2,0], 50), rpg.Character("Medusa"
 
 
 # Game main loop
-print "\n\n Battle has started!"
+print("\n\n Battle has started!")
 
 while not check_death(foes):
     index = 0
     while index < len(foes):
-        print "{0}. {1} \t [{2} HP]".format(index+1, foes[index].name, foes[index].hp)
+        print("{0}. {1} \t [{2} HP]".format(index+1, foes[index].name, foes[index].hp))
         index = index + 1
-    choice = input("Attack enemy: ")
+    choice = int(input("Attack enemy: "))
     foes[choice-1].get_hit(23)
-    print "\n\n"
-print "\n Victory Fare ~~~~"
+    print("\n\n")
+print("\n Victory Fare ~~~~")
--- rpg.py
+++ rpg.py
@@ -1,4 +1,7 @@
-class Character():
+import random
+
+
+class Character:
     def __init__(self, name, stats, hp):
         self.name = name
         self.hp = hp
@@ -8,11 +11,16 @@ class Character():
         self.stats_int = stats[3]
         self.stats_wis = stats[4]
         self.stats_chm = stats[5]
-        print "A '{0}' has been born, with {1}hp!".format(name,hp)
+        print ("A '{0}' has been born, with {1}hp!".format(name,hp))
 
     def get_hit(self, damage):
         self.hp -= damage
-        print "{0} has recieved {1} damage!".format(self.name, damage)
+        print ("{0} has recieved {1} damage!".format(self.name, damage))
         if self.hp <= 0:
             self.hp = 0
-            print "{0} has been defeated!"
+            print ("{0} has been defeated!")
+
+    def attack(self):
+        dice = random.randint(1,12)
+        damage = self.stats_str + dice
+        return damage
